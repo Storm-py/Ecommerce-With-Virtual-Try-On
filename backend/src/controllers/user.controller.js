450c2e7176c1acbceebba20f856185d2a3f81bb3
Storm-py <ranamuneebmay2018@gmail.com>
2025-10-14 19:14:28 +0500

    Solved Some Errors

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -10,6 +10,9 @@ import { Product } from "../models/product.model.js";
 import Stripe from 'stripe'
 import { Order } from "../models/order.model.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const stripe=new Stripe(process.env.STRIPE_SECRET_KEY)
 
