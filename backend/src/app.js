510b59f12ab1be25907e692d8e795e61c71458b2
Storm-py <ranamuneebmay2018@gmail.com>
2025-10-15 02:08:36 +0500

    working

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,42 +1,31 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 import express, { urlencoded } from 'express'
+import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
 const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+    methods: ['GET','POST','PUT','DELETE','PATCH'],
+}))
 
-app.use((req, res, next) => {
-    const allowedOrigins = [
-        'https://ecommerce-website-self-five.vercel.app',
-        'http://localhost:5173',
-        process.env.CORS_ORIGIN
-    ];
-    
-    const origin = req.headers.origin;
-    if (allowedOrigins.includes(origin)) {
-        res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Cookie');
-    
-    if (req.method === 'OPTIONS') {
-        return res.status(200).end();
-    }
-    
-    next();
-});
 
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.static('public'))
-app.use(urlencoded({extended: true}))
+app.use(urlencoded({extended:true}))
+
 
 import userRouter from './routes/userRoutes.js'
 import adminRouter from './routes/adminRoutes.js'
 
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/admin", adminRouter)
+app.use("/api/v1/users",userRouter)
+app.use("/api/v1/admin",adminRouter)
 
-export { app }
+
+
+
+export {app}
